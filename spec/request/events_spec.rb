77782970248d068de6bebd1c03f7b57f712e3ef8
Beau Dacious <dacious.beau@gmail.com>
2014-09-10 22:46:48 -0700

    write failing /events spec

--- spec/request/events_spec.rb
+++ spec/request/events_spec.rb
@@ -3,6 +3,16 @@ require "spec_helper"
 describe "/" do
   it "is OK" do
     get "/"
+
     expect(last_response).to be_ok
+  end
+end
+
+describe "/events" do
+  it "returns a list of events" do
+    get "/events"
+
+    expect(last_response).to be_ok
+    expect(json).to eq events_json
   end
 end
